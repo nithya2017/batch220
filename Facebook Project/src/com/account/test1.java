7032ad992c6fad5be774d82945c5da1b7de3a6fd
sdsubbu <sdsubbu@SDS.fios-router.home>
2017-04-04 16:36:41 -0400

    changed test1

---
+++ Facebook Project/src/com/account/test1.java
@@ -0,0 +1,23 @@
+package com.account;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+//testing	
+	
+	
+	public static WebDriver driver=null;
+	
+	
+
+	
+	public static void openBrowser(){
+		
+		
+		driver=new ChromeDriver();
+	
+	}
+
+}
